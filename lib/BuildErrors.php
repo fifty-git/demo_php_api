d1d54a25317cf28a3a252a8038538bdbafefe8d1
donstefani <don.stefani@gmail.com>
2017-12-19 10:01:17 -0700

    built out error configuration and returning

--- lib/BuildErrors.php
+++ lib/BuildErrors.php
@@ -3,6 +3,7 @@
 namespace Lib;
 
 use Lib\SqlModel;
+use Crell\ApiProblem\ApiProblem;
 
 /**
  * summary
@@ -11,12 +12,39 @@ class BuildErrors
 {
     private $sqlHandler;
 
-    public function __construct()
+    public function __construct(SqlModel $sqlHandler)
     {
-        $this->sqlHandler = new SqlModel();
+        $this->sqlHandler = $sqlHandler;
     }
 
-    public function getJsonError($response)
+    public function getJsonError($response, $requestUri, $errorCode)
     {
+        $errorData = $this->getErrorDetailsSql($errorCode);
+        
+        $errorType = $errorData['er_error_type'];
+        $errorUrl = "https://errors.fiftyflowers.com/$errorCode";
+        $errorMessage = $errorData['er_error_message'];
+        $setStatus = (int)$errorData['er_status_code'];
+
+        $problem = new ApiProblem($errorType, $errorUrl);
+        $problem->setDetail($errorMessage);
+        $problem->setInstance($requestUri);
+        $newResponse = $response
+          ->withHeader('Content-type', 'application/json')
+          ->withStatus($setStatus);
+        $newResponse->write($problem->asJson());
+
+        return $newResponse;
+    }
+
+    private function getErrorDetailsSql($errorCode)
+    {
+        $sql = "SELECT * 
+        FROM api_error_data 
+        WHERE er_error_code = :errorCode";
+
+        $bindValues = array('errorCode'=>$errorCode);
+        $errorData = $this->sqlHandler->fetchFirstRow($sql, $bindValues);
+        return $errorData;
     }
 }
